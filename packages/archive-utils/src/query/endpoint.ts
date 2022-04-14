9ac1d44c551d345f4e6800ed4291cf038b164eb0
Ben Mason <ben@wattmason.co.uk>
2022-04-14 17:33:20 +0100

    Make graph endpoints optional

--- packages/archive-utils/src/query/endpoint.ts
+++ packages/archive-utils/src/query/endpoint.ts
@@ -1,9 +1,21 @@
+import colors from 'colors';
+
 if (!process.env.ARCHIVE_GRAPH) {
   console.log(
-    'process.env.ARCHIVE_GRAPH to be set, ignore if you are not querying the archive'
+    `\n${colors.bgMagenta(
+      'process.env.ARCHIVE_GRAPH should be set if you are querying the archive'
+    )}\n`
+  );
+}
+if (!process.env.CONTRACT_GRAPH) {
+  console.log(
+    `\n${colors.bgMagenta(
+      'process.env.CONTRACT_GRAPH should be set if you are querying the contracts archive'
+    )}\n`
   );
 }
 
 const endpoint = process.env.ARCHIVE_GRAPH!;
+export const contractEndpoint = process.env.CONTRACT_GRAPH!;
 
 export default endpoint;
