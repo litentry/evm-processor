d80cad68bb992e945cf35de7ad74c5615dea01d8
Ben Mason <ben@wattmason.co.uk>
2022-04-14 17:42:51 +0100

    make RPC endpoint optional

--- packages/archive-utils/src/web3.ts
+++ packages/archive-utils/src/web3.ts
@@ -1,16 +1,21 @@
 import Web3 from 'web3';
+import colors from 'colors';
 import { WebsocketProvider, HttpProvider } from 'web3-core';
 
 if (!process.env.RPC_ENDPOINT) {
-  throw new Error('process.env.RPC_ENDPOINT must be an RPC provider');
+  console.log(
+    `\n${colors.bgMagenta(
+      'process.env.RPC_ENDPOINT must be an RPC provider, ignore this if you are not making chain node calls in your indexer'
+    )}\n`
+  );
 }
 
 let provider: WebsocketProvider | HttpProvider;
 
-if (process.env.RPC_ENDPOINT.startsWith('ws')) {
+if (process.env.RPC_ENDPOINT?.startsWith('ws')) {
   provider = new Web3.providers.WebsocketProvider(process.env.RPC_ENDPOINT);
 } else {
-  provider = new Web3.providers.HttpProvider(process.env.RPC_ENDPOINT);
+  provider = new Web3.providers.HttpProvider(process.env.RPC_ENDPOINT!);
 }
 
 const web3 = new Web3(provider);
