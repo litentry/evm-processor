d640f5d0f936d48b14fd5bb8a5bce09f6a7a8fcd
Ben Mason <ben@wattmason.co.uk>
2022-04-20 22:32:50 +0100

    Fix port default typo

--- indexers/token-activity/src/config.ts
+++ indexers/token-activity/src/config.ts
@@ -1,6 +1,6 @@
 import { query } from 'indexer-utils';
 
-const port = process.env.PORT ? parseInt(process.env.PORT) : 4051;
+const port = process.env.PORT ? parseInt(process.env.PORT) : 4052;
 const start = process.env.START_BLOCK ? parseInt(process.env.START_BLOCK) : 0;
 const end = process.env.END_BLOCK
   ? parseInt(process.env.END_BLOCK)
