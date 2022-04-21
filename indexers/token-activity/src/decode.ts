7f95afaf082f32fe13b9d88202e57e94ed1a2869
Ben Mason <ben@wattmason.co.uk>
2022-04-21 12:42:28 +0100

    Handle conflicting signatures

--- indexers/token-activity/src/decode.ts
+++ indexers/token-activity/src/decode.ts
@@ -5,6 +5,32 @@ import ERC721 from '@openzeppelin/contracts/build/contracts/ERC721.json';
 import ERC1155 from '@openzeppelin/contracts/build/contracts/ERC1155.json';
 import { ContractType } from 'indexer-utils/lib/types/contract';
 
+export type DecodedEvent = {
+  value1?: string;
+  value2?: string;
+  value3?: string;
+  value4?: string;
+  type1?: string;
+  type2?: string;
+  type3?: string;
+  type4?: string;
+};
+
+export type DecodedExtrinsic = {
+  value1?: string;
+  value2?: string;
+  value3?: string;
+  value4?: string;
+  value5?: string;
+  value6?: string;
+  type1?: string;
+  type2?: string;
+  type3?: string;
+  type4?: string;
+  type5?: string;
+  type6?: string;
+};
+
 const contractInterface: {
   [key: string]: ethers.utils.Interface;
 } = {
@@ -18,71 +44,43 @@ export function decodeEvent(
   item: Types.Contract.ContractSignatureItem,
   data: string,
   topics: string[]
-) {
+): DecodedEvent {
   const decoded = contractInterface[`ERC${type}`].decodeEventLog(
     item.SIGNATURE,
     data,
     topics
   );
 
-  return decoded.reduce(
-    (data, value, i) => {
-      let val: string = value.toString();
-      if (item.PARAMS[i] === 'address') {
-        val = val.toLowerCase();
-      }
-      return {
-        ...data,
-        [`value${i + 1}`]: val,
-        [`type${i + 1}`]: item.PARAMS[i],
-      };
-    },
-    {} as {
-      value1?: string;
-      value2?: string;
-      value3?: string;
-      value4?: string;
-      type1?: string;
-      type2?: string;
-      type3?: string;
-      type4?: string;
+  return decoded.reduce((data, value, i) => {
+    let val: string = value.toString();
+    if (item.PARAMS[i] === 'address') {
+      val = val.toLowerCase();
     }
-  );
+    return {
+      ...data,
+      [`value${i + 1}`]: val,
+      [`type${i + 1}`]: item.PARAMS[i],
+    };
+  }, {} as DecodedEvent);
 }
 
 export function decodeParams(
   params: Types.Contract.ContractSignatureItem['PARAMS'],
   input: string
-) {
+): DecodedExtrinsic {
   const decoded = ethers.utils.defaultAbiCoder.decode(
     params,
     ethers.utils.hexDataSlice(input, 4)
   );
-  return decoded.reduce(
-    (data, value, i) => {
-      let val: string = value.toString();
-      if (params[i] === 'address') {
-        val = val.toLowerCase();
-      }
-      return {
-        ...data,
-        [`value${i + 1}`]: val,
-        [`type${i + 1}`]: params[i],
-      };
-    },
-    {} as {
-      value1?: string;
-      value2?: string;
-      value3?: string;
-      value4?: string;
-      value5?: string;
-      value6?: string;
-      type1?: string;
-      type2?: string;
-      type3?: string;
-      type4?: string;
-      type5?: string;
-      type6?: string;
+  return decoded.reduce((data, value, i) => {
+    let val: string = value.toString();
+    if (params[i] === 'address') {
+      val = val.toLowerCase();
     }
-  );
+    return {
+      ...data,
+      [`value${i + 1}`]: val,
+      [`type${i + 1}`]: params[i],
+    };
+  }, {} as DecodedExtrinsic);
 }
--- indexers/token-activity/src/events-handler.ts
+++ indexers/token-activity/src/events-handler.ts
@@ -1,10 +1,16 @@
 import { query, Types } from 'indexer-utils';
-import { decodeEvent } from './decode';
+import { decodeEvent, DecodedEvent } from './decode';
 import {
   ERC1155EventDecodedModel,
   ERC20EventDecodedModel,
   ERC721EventDecodedModel,
 } from './schema';
+
+const CONFLICTING_SIGNATURES = [
+  'Transfer(address,address,uint256)',
+  'Approval(address,address,uint256)',
+  'ApprovalForAll(address,address,bool)',
+];
 
 export default async function eventsHandler(
   startBlock: number,
@@ -47,23 +53,35 @@ export default async function eventsHandler(
     .filter((log) => ercContractAddresses.includes(log.address));
 
   await model.insertMany(
-    ercLogs.map((log) => {
-      const sig = sigs.find((sig) => [sig.ID, sig._ID].includes(log.topic0))!;
-      return {
-        contract: log.address,
-        blockNumber: log.blockNumber,
-        blockTimestamp: log.blockTimestamp,
-        transactionHash: log.transactionHash,
-        signature: sig.SIGNATURE,
-        ...decodeEvent(
-          type,
-          sig,
-          log.data,
-          [log.topic0, log.topic1, log.topic2, log.topic3, log.topic4].filter(
-            (t) => t
-          ) as string[]
-        ),
-      } as Types.Contract.DecodedContractEvent;
-    })
+    ercLogs
+      .map((log) => {
+        const sig = sigs.find((sig) => [sig.ID, sig._ID].includes(log.topic0))!;
+        let decoded: DecodedEvent;
+        try {
+          decoded = decodeEvent(
+            type,
+            sig,
+            log.data,
+            [log.topic0, log.topic1, log.topic2, log.topic3, log.topic4].filter(
+              (t) => t
+            ) as string[]
+          );
+        } catch (e) {
+          // contracts can be more than 1 standard, when the same methods are found on both we know this will blow up for 1 of the contract types as the log data won't match up (e.g. unit256 on transfer is indexed in 721 but not 20)
+          if (CONFLICTING_SIGNATURES.includes(sig.SIGNATURE)) {
+            return null;
+          }
+          throw new Error(JSON.stringify(e));
+        }
+        return {
+          contract: log.address,
+          blockNumber: log.blockNumber,
+          blockTimestamp: log.blockTimestamp,
+          transactionHash: log.transactionHash,
+          signature: sig.SIGNATURE,
+          ...decoded,
+        } as Types.Contract.DecodedContractEvent;
+      })
+      .filter((log) => log)
   );
 }
