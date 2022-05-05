cc8ebfe5ab92d4dde72daab6b72a6faac8d205a6
Jeff Horner <jeff@stratic.io>
2022-05-05 09:49:24 +0100

    wip: add mongo container support

--- indexers/archive/serverless.ts
+++ indexers/archive/serverless.ts
@@ -1,21 +1,41 @@
 import producer from '@functions/producer';
 import worker from '@functions/worker';
 import type { AWS } from '@serverless/typescript';
+import containerResources from "./serverless/config/container-resources";
 import stageConfigFactory from './serverless/config/stage-config';
 import {getContext} from "./serverless/util/context";
 
 const context = getContext();
 const stageConfig = stageConfigFactory(context.options.stage);
 
+export type Params = {
+    org: string;
+    clusterStackName: string;
+    mongoImageVersion: string;
+    region: AWS['provider']['region'];
+    mongoDnsName: string;
+    ebsVolumeName: string;
+}
+
+const serviceName = 'archive-indexer';
+
+const params: Params = {
+    org: 'litentry',
+    clusterStackName: 'graph-aws-infrastructure-dev',
+    mongoImageVersion: '5.0.8',
+    region: 'eu-west-1',
+    mongoDnsName: `${serviceName}-mongo`,
+    ebsVolumeName: `${serviceName}-mongo-ebs`
+}
 
 const serverlessConfiguration: AWS = {
-    service: 'archive-indexer',
+    service: serviceName,
     frameworkVersion: '3',
     plugins: ['serverless-esbuild', 'serverless-localstack'],
     provider: {
         name: 'aws',
         runtime: 'nodejs14.x',
-        region: 'eu-west-1',
+        region: params.region,
         apiGateway: {
             minimumCompressionSize: 1024,
             shouldStartNameWithService: true,
@@ -23,6 +43,7 @@ const serverlessConfiguration: AWS = {
         environment: {
             AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
             NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+            MONGO_URI: `mongodb://${params.mongoDnsName}.${params.org}:27017/archive`
         },
         iam: {
             role: {
@@ -64,12 +85,7 @@ const serverlessConfiguration: AWS = {
                     VisibilityTimeout: 60
                 }
             },
-            ProducerBucket: {
-                Type: 'AWS::S3::Bucket',
-                Properties: {
-                    BucketName: stageConfig.getProducerBucketName()
-                }
-            }
+            ...containerResources(params).Resources
         }
     },
     functions: {
