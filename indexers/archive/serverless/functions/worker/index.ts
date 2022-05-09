a72f074bdab54cd3515589b6ded9d44ee0422700
Mark Davison <github@iammark.co.uk>
2022-05-09 09:18:17 +0100

    Get mongodb running locally and add stage config (#19)
    * Get mongodb running locally and add stage config
    
    * add a comment
    
    * Add packages
    
    * add s3 persistance
    
    * Remove accidental commit

--- indexers/archive/serverless/functions/worker/index.ts
+++ indexers/archive/serverless/functions/worker/index.ts
@@ -1,7 +1,13 @@
 import { handlerPath } from '@libs/handler-resolver';
+import stageConfigFactory from '../../config/stage-config';
+import {getContext} from "../../util/context";
+
+const context = getContext();
+const stageConfig = stageConfigFactory(context.options.stage);
 
 export default {
     handler: `${handlerPath(__dirname)}/handler.default`,
+    reservedConcurrency: stageConfig.getWorkerConcurrency(),
     events: [
         {
             sqs: {
@@ -15,7 +21,8 @@ export default {
     ],
     environment: {
         QUEUE_URL: { Ref: 'JobQueue' },
-        RPC_ENDPOINT: 'https://rpc.ankr.com/eth'
+        RPC_ENDPOINT: 'https://rpc.ankr.com/eth',
+        MONGO_URI: stageConfig.getMongoURI()
     },
     timeout: 20,
 };
