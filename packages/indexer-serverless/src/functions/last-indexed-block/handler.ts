ca362e52c6bc93ef44e8aef5d8b33bebba808325
Ben Mason <ben@wattmason.co.uk>
2022-05-23 21:45:30 +0100

    add last indexed block calculation

--- packages/indexer-serverless/src/functions/last-indexed-block/handler.ts
+++ packages/indexer-serverless/src/functions/last-indexed-block/handler.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
-// import { calculateLastIndexedBlock } from 'indexer-utils/lib/last-indexed-block';
+import { repository } from 'indexer-utils';
 
 export default async () => {
   await mongoose.connect(process.env.MONGO_URI!);
-  // await calculateLastIndexedBlock();
+  await repository.lastIndexedBlock.calculate();
   await mongoose.disconnect();
 };
--- packages/indexer-utils/src/repository/last-indexed-block.ts
+++ packages/indexer-utils/src/repository/last-indexed-block.ts
@@ -1,4 +1,9 @@
 import mongoose from 'mongoose';
+import {
+  remove as removeIndexedBlockRanges,
+  get as getIndexedBlockRanges,
+  IndexedBlockRangeDocument,
+} from './indexed-block-range';
 
 interface LastIndexedBlockDocument extends mongoose.Document {
   lastIndexedBlock: number;
@@ -27,4 +32,25 @@ export const save = async (lastIndexedBlock: number): Promise<void> => {
   }
 
   await currentValue.updateOne({ lastIndexedBlock });
+};
+
+export const calculate = async () => {
+  let lastIndexedBlock = await get();
+  const processedIndexedBlockRanges: IndexedBlockRangeDocument[] = [];
+  const pendingIndexedBlockRanges = await getIndexedBlockRanges();
+
+  pendingIndexedBlockRanges.forEach((pendingIndexedBlockRange) => {
+    if ((lastIndexedBlock || 0) + 1 < pendingIndexedBlockRange.startBlock) {
+      return;
+    }
+
+    processedIndexedBlockRanges.push(pendingIndexedBlockRange);
+
+    lastIndexedBlock = pendingIndexedBlockRange.endBlock;
+  });
+
+  if (processedIndexedBlockRanges.length > 0 && lastIndexedBlock !== null) {
+    await save(lastIndexedBlock);
+    await removeIndexedBlockRanges(processedIndexedBlockRanges);
+  }
 };
