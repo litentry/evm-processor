df1b8395854f33bf67eb2dd967ab99bdfe3e9bfe
Mark Davison <github@iammark.co.uk>
2022-05-27 20:00:38 +0100

    Improve call-x-times-over-y-seconds (#49)

--- packages/indexer-utils/src/utils/call-x-times-over-y-seconds.ts
+++ packages/indexer-utils/src/utils/call-x-times-over-y-seconds.ts
@@ -1,13 +1,30 @@
-export default (iterations: number, maxTime: number, fn: () => any) =>
+export default (targetInvocations: number, maxTime: number, fn: () => any) =>
   new Promise((resolve) => {
-    const interval = (maxTime * 1000) / iterations;
+    let invocation = 0;
+    const startTime = new Date().getTime();
+    const endTime = startTime + (maxTime * 1000)
 
-    const callFn = () => {
-      fn();
-      if (--iterations <= 0) {
+    const callFn = async () => {
+      invocation++
+      const invocationStartTime = new Date().getTime();
+      await fn();
+      const invocationEndTime = new Date().getTime();
+      const invocationTime = invocationEndTime - invocationStartTime;
+      console.log(`Invocation ${invocation} of ${targetInvocations} took ${invocationTime}ms`);
+
+      if (invocation >= targetInvocations) {
         return resolve(null);
       }
-      setTimeout(callFn, interval);
+
+      const remainingTime = endTime - new Date().getTime();
+      const predictedExecutionTimeRemaining = invocationTime * invocation;
+      const waitTime = (remainingTime - predictedExecutionTimeRemaining) / (targetInvocations - invocation);
+
+      if (waitTime < 0) {
+        console.log(`Command is running behind schedule - maybe reduce the number of invocations?`);
+      }
+
+      setTimeout(callFn, waitTime);
     };
 
     callFn();
