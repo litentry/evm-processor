2b316d7ebe44445cff8a65d80db34515784ec420
João D <joao@litentry.com>
2022-05-30 11:26:45 +0100

    Parallel get block receipts

--- indexers/archive/src/indexer/extract-block.ts
+++ indexers/archive/src/indexer/extract-block.ts
@@ -3,19 +3,17 @@ import { web3 } from 'indexer-utils';
 import { ExtractBlock } from './types';
 
 async function getReceipts(transactionHashes: string[]) {
-  const receipts = await Promise.all(
-    transactionHashes.map(async (hash) => {
-      const receipt = await web3.eth.getTransactionReceipt(hash);
+  const receipts = transactionHashes.map(async (hash) => {
+    const receipt = await web3.eth.getTransactionReceipt(hash);
 
-      if (!receipt) {
-        // some providers fail to provide receipts
-        throw new Error(
-          `Failed to fetch receipt for tx: ${hash}. You must provide an RPC_ENDPOINT for a full archive node.`,
-        );
-      }
-      return receipt;
-    }),
-  );
+    if (!receipt) {
+      // some providers fail to provide receipts
+      throw new Error(
+        `Failed to fetch receipt for tx: ${hash}. You must provide an RPC_ENDPOINT for a full archive node.`,
+      );
+    }
+    return receipt;
+  });
   return receipts;
 }
 
