5ea06ac516cace93787810e7578138e792ccde6f
João D <joao@litentry.com>
2022-06-02 15:48:53 +0100

    Change upsert

--- packages/indexer-utils/src/utils/upsert-mongo-models.ts
+++ packages/indexer-utils/src/utils/upsert-mongo-models.ts
@@ -6,22 +6,29 @@ export default async function upsertMongoModels(
   primaryKey: string[],
 ): Promise<void> {
   if (documents.length > 0) {
-    await model.bulkWrite(
-      documents.map((document) => ({
-        updateOne: {
-          filter: primaryKey.reduce(
-            (filter, field) => ({
-              ...filter,
-              [field]: document[field],
-            }),
-            {},
-          ),
-          update: {
-            ...document,
+    try {
+      // First we try inserting here but there might be clashes
+      await model.insertMany(documents);
+    } catch (error) {
+      // If we fail on the above, we try again with the
+      // initial implementation to upsert the documents.
+      await model.bulkWrite(
+        documents.map((document) => ({
+          updateOne: {
+            filter: primaryKey.reduce(
+              (filter, field) => ({
+                ...filter,
+                [field]: document[field],
+              }),
+              {},
+            ),
+            update: {
+              ...document,
+            },
+            upsert: true,
           },
-          upsert: true,
-        },
-      })),
-    );
+        })),
+      );
+    }
   }
 }
