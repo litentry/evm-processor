ed102065414eab24eae0f23ac1741333f06a2b53
Jeff Horner <jeff@stratic.io>
2022-06-06 12:34:58 +0100

    fix: test config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   clearMocks: true,
   setupFilesAfterEnv: ['./setup-jest.ts'],
-  testRegex: [
-    '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$'
-  ],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
