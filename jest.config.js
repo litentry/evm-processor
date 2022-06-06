8b1006a70fb697fb796312043dacf53d86304f96
Jeff Horner <jeff@stratic.io>
2022-06-06 10:56:09 +0100

    fix: jest config should only test .ts files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   clearMocks: true,
   setupFilesAfterEnv: ['./setup-jest.ts'],
+  testRegex: [
+    '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$'
+  ],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
