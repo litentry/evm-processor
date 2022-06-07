3e12eeda56b42083a46c758985af1604382914b9
João D <joao@litentry.com>
2022-06-07 21:07:30 +0100

    Insert + upsert

--- packages/indexer-utils/src/utils/upsert-mongo-models.ts
+++ packages/indexer-utils/src/utils/upsert-mongo-models.ts
@@ -1,27 +1,39 @@
 import mongoose from 'mongoose';
 
+const upsert = async (
+  model: mongoose.Model<any>,
+  documents: any[],
+  primaryKey: string[],
+): Promise<void> => {
+  await model.bulkWrite(
+    documents.map((document) => ({
+      updateOne: {
+        filter: primaryKey.reduce(
+          (filter, field) => ({
+            ...filter,
+            [field]: document[field],
+          }),
+          {},
+        ),
+        update: {
+          ...document,
+        },
+        upsert: true,
+      },
+    })),
+  );
+};
+
 export default async function upsertMongoModels(
   model: mongoose.Model<any>,
   documents: any[],
   primaryKey: string[],
 ): Promise<void> {
   if (documents.length > 0) {
-    await model.bulkWrite(
-      documents.map((document) => ({
-        updateOne: {
-          filter: primaryKey.reduce(
-            (filter, field) => ({
-              ...filter,
-              [field]: document[field],
-            }),
-            {},
-          ),
-          update: {
-            ...document,
-          },
-          upsert: true,
-        },
-      })),
-    );
+    try {
+      await model.collection.insertMany(documents);
+    } catch (error) {
+      await upsert(model, documents, primaryKey);
+    }
   }
 }
