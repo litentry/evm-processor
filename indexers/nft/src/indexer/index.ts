fed037bd4bf1cb3b04a8642e39fdb190d35e5d45
Ben Mason <ben@wattmason.co.uk>
2022-06-07 18:39:20 +0100

    Add indexer

--- indexers/nft/src/indexer/index.ts
+++ indexers/nft/src/indexer/index.ts
@@ -1,3 +1,10 @@
-export default async function handler(start: number, end: number) {
-  return;
+import { repository } from 'indexer-utils';
+import extract from './extract';
+import transform from './transform';
+import load from './load';
+
+export default async function indexer(startBlock: number, endBlock: number) {
+  const data = await extract(startBlock, endBlock);
+  await load(transform(data));
+  await repository.indexedBlockRange.save(startBlock, endBlock);
 }
