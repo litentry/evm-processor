53b3e196cffd12b461b26688e334852d4f69ee0a
Ben Mason <ben@wattmason.co.uk>
2022-06-16 12:57:31 +0100

    update token activity producer

--- indexers/token-activity/serverless.ts
+++ indexers/token-activity/serverless.ts
@@ -5,7 +5,6 @@ module.exports = serverless({
   serviceName: 'token-activity',
   chain: <Chain>process.env.CHAIN!,
   version: process.env.DEPLOY_VERSION!,
-  latestBlockDependency: 'contract-graph',
   rpcEndpoint: process.env.RPC_ENDPOINT,
   archiveGraph: process.env.ARCHIVE_GRAPH,
   contractGraph: process.env.CONTRACT_GRAPH,
