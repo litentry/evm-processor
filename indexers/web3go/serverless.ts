ac498194f08417327a99b80a373cc345c5357719
Ben Mason <ben@wattmason.co.uk>
2022-06-21 11:24:28 +0100

    remove unused env

--- indexers/web3go/serverless.ts
+++ indexers/web3go/serverless.ts
@@ -5,7 +5,6 @@ module.exports = serverless({
   serviceName: 'web3go',
   chain: <Chain>process.env.CHAIN!,
   version: process.env.DEPLOY_VERSION!,
-  rpcEndpoint: process.env.RPC_ENDPOINT,
   archiveGraph: process.env.ARCHIVE_GRAPH,
   contractGraph: process.env.CONTRACT_GRAPH,
   tokenActivityGraph: process.env.TOKEN_ACTIVITY_GRAPH,
