4da6f99808e4a5d4e7b5241094b4baaf4989ec50
Ben Mason <ben@wattmason.co.uk>
2022-06-22 10:59:20 +0100

    Handle weird left padded addresses

--- indexers/token-activity/src/indexer/decode.ts
+++ indexers/token-activity/src/indexer/decode.ts
@@ -71,8 +71,9 @@ export function decodeParams(
 ): DecodedExtrinsic {
   const decoded = ethers.utils.defaultAbiCoder.decode(
     params,
-    ethers.utils.hexDataSlice(input, 4),
+    ethers.utils.hexDataSlice(handleWeirdLeftPaddedAddresses(input, params), 4),
   );
+
   return decoded.reduce((data, value, i) => {
     let val: string = value.toString();
     if (params[i] === 'address') {
@@ -84,4 +85,25 @@ export function decodeParams(
       [`type${i + 1}`]: params[i],
     };
   }, {} as DecodedExtrinsic);
+}
+
+/*
+Some ERC20 transfer(address, unit256) transactions (e.g. https://etherscan.io/tx/0x92a0d3e9190cdb9ae986e95a922452bb864835130e7c382265a6c6689d71fa19) left pad the receiver address with apparently random characters instead of the usual zeros. This results in ethers.utils.defaultAbiCoder.decode giving us a Getter value that throws this when accessed:
+
+value out of range (argument="value", value=20, code=INVALID_ARGUMENT, version=bytes/5.1.0)
+
+Replacing the odd padding with zeros fixes this issue, at least for this particular extrinsic.
+*/
+function handleWeirdLeftPaddedAddresses(
+  input: string,
+  params: Types.Contract.ContractSignatureItem['PARAMS'],
+): string {
+  let formatted = input;
+
+  if (params[0] === 'address') {
+    formatted =
+      input.substring(0, 10) + '000000000000000000000000' + input.substring(34);
+  }
+
+  return formatted;
 }
