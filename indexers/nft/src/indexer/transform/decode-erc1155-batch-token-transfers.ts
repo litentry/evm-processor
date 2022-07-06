b31023f4a599c1b2d47a17f98260c3da5f1f3778
Ben Mason <ben@wattmason.co.uk>
2022-07-06 06:54:09 +0100

    Handle non compliant NFT logs

--- indexers/nft/src/indexer/transform/decode-erc1155-batch-token-transfers.ts
+++ indexers/nft/src/indexer/transform/decode-erc1155-batch-token-transfers.ts
@@ -3,40 +3,53 @@ import BigNumber from 'bignumber.js';
 import { ethers } from 'ethers';
 import { Types } from 'indexer-utils';
 
+type Decoded = [string, string, string, BigNumber[], BigNumber[]];
+
 export default function decodeErc1155BatchTokenTransfers(
   logs: Types.Archive.Log[],
   contracts: Types.Contract.ERC1155Contract[],
 ): Types.Nft.ERC1155TokenTransfer[] {
-  return logs
-    .map((log) => {
-      const decoded = new ethers.utils.Interface(ERC1155.abi).decodeEventLog(
+  return logs.reduce((prev, log) => {
+    let decoded: Decoded;
+
+    try {
+      decoded = new ethers.utils.Interface(ERC1155.abi).decodeEventLog(
         'TransferBatch(address,address,address,uint256[],uint256[])',
         log.data,
         [log.topic0, log.topic1!, log.topic2!, log.topic3!],
+      ) as Decoded;
+    } catch (e) {
+      console.error(
+        `Non compliant log on tx: ${log.transactionHash} at index ${log.logIndex}`,
       );
+      /*
+      Some transactions emit dodgy logs e.g. https://etherscan.io/tx/0x043f5291b40c6b8c47f6e5578942ac60bed0284b10de7baf9394317b0b70ec1f
+      */
+      return prev;
+    }
 
-      const shared = {
-        _id: log._id,
-        contract: log.address,
-        from: (decoded[1] as string).toLowerCase(),
-        to: (decoded[2] as string).toLowerCase(),
-        transactionHash: log.transactionHash,
-        transactionId: log.transactionId,
-        blockNumber: log.blockNumber,
-        blockTimestamp: log.blockTimestamp,
-        collectionName: contracts.find(
-          (contract) => contract._id === log.address,
-        )?.name,
+    const shared = {
+      _id: log._id,
+      contract: log.address,
+      from: (decoded[1] as string).toLowerCase(),
+      to: (decoded[2] as string).toLowerCase(),
+      transactionHash: log.transactionHash,
+      transactionId: log.transactionId,
+      blockNumber: log.blockNumber,
+      blockTimestamp: log.blockTimestamp,
+      collectionName: contracts.find((contract) => contract._id === log.address)
+        ?.name,
+    };
+
+    const transfers = (decoded[3] as BigNumber[]).map((tokenId, i) => {
+      const transfer: Types.Nft.ERC1155TokenTransfer = {
+        tokenId: tokenId.toString(),
+        quantity: (decoded[4][i] as BigNumber).toNumber(),
+        ...shared,
       };
+      return transfer;
+    });
 
-      return (decoded[3] as BigNumber[]).map((tokenId, i) => {
-        const transfer: Types.Nft.ERC1155TokenTransfer = {
-          tokenId: tokenId.toString(),
-          quantity: (decoded[4][i] as BigNumber).toNumber(),
-          ...shared,
-        };
-        return transfer;
-      });
-    })
-    .flat();
+    return [...prev, ...transfers];
+  }, [] as Types.Nft.ERC1155TokenTransfer[]);
 }
