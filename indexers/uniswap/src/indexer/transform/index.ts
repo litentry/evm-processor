50b4028f60831a59a05f9497ecf75afe95ad38ac
Ben Mason <ben@wattmason.co.uk>
2022-07-07 13:20:45 +0100

    Fix bug in transforming swaps

--- indexers/uniswap/src/indexer/transform/index.ts
+++ indexers/uniswap/src/indexer/transform/index.ts
@@ -1,9 +1,11 @@
-import transformV2Swaps from './transform-v2-swaps';
-import transformMulticall from './transform-multicall';
 import { ExtractedData, Swap } from '../types';
+import transformMulticall from './transform-multicall';
+import transformV2Swaps from './transform-v2-swaps';
 
 export default function transform({ v2, v3 }: ExtractedData) {
-  const v2Swaps = v2.map((item) => transformV2Swaps[item.method](item.txs[0]));
+  const v2Swaps = v2.flatMap((item) =>
+    item.txs.map((tx) => transformV2Swaps[item.method](tx)),
+  );
   const v3Swaps = v3
     .map((tx) => transformMulticall(tx))
     .filter((swap) => swap) as Swap[];
